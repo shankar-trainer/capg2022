f1216d64c30661573314fec801e4b82ab0d124a6
shankar-trainer <shankar7979@hotmail.com>
2022-03-22 16:08:01 +0530

    factorial

---
+++ factorial.java
@@ -0,0 +1,12 @@
+class factorial{
+  public static void main(String args[])    {
+    int x=5;
+    int fact=1;
+  
+  for(int p=1;p<=x;p++)
+      fact=fact*p;
+
+   System.out.println("factorial of "+x+" is "+fact);
+    }
+
+}
